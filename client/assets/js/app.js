fbef8abf8dc9a783df552479a7c46f14dd92c86b
Christian <christian_hane@aol.com>
2018-03-22 21:37:45 -0700

    commit message

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,28 +1,30 @@
-$('.input').keypress(async function (e) {
-  if(e.which == 13) {
-    try {
-      const city = $('.cityName').val().trim();
-      const cityLocaion = await getLatLng(city);
-      console.log(cityLocaion);
-      localStorage.setItem('location', JSON.stringify(cityLocaion));
-      window.location.replace('/events');
-    } catch(error) {
-      if (error.message) {
-        console.error(error.message);
+$(document).ready(function() {
+  $('.input').keypress(async function(event) {
+    if(event.which == 13) {
+      try {
+        const city = $('.landingInput').val().trim();
+        const cityLocaion = await getLatLng(city);
+        localStorage.setItem('location', JSON.stringify(cityLocaion));
+        $.get('/events?lat=' + cityLocation.lat + '&lng=' + cityLocation.lng);
+      } catch(error) {
+        if (error.message) {
+          console.error(error.message);
+        }
+        console.error(error);
       }
-      console.error(error);
     }
+  })
+  
+  function getLatLng(city) {
+    return new Promise((resolve, reject) => {
+      $.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyDsfnjM905ho9lC-EwFVAI8oOUivynhT9g`, (error, response, body) =>  {
+        const location = {
+          lat: body.responseJSON.results[0].geometry.location.lat,
+          lng: body.responseJSON.results[0].geometry.location.lng,
+        }
+        resolve(location);
+      })  
+    })
   }
 })
 
-function getLatLng(city) {
-  return new Promise((resolve, reject) => {
-    $.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyDsfnjM905ho9lC-EwFVAI8oOUivynhT9g`, (error, response, body) =>  {
-      const location = {
-        lat: body.responseJSON.results[0].geometry.location.lat,
-        lng: body.responseJSON.results[0].geometry.location.lng,
-      }
-      resolve(location);
-    })  
-  })
-}
--- client/assets/js/results.js
+++ client/assets/js/results.js
@@ -1,9 +1,9 @@
 async function initMap() {
-  const allEvents = await getAllEvents(); 
-  const location = JSON.parse(localStorage.getItem('location'));
+  const location = JSON.parse(localStorage.getItem('location'));  
+  const allEvents = await getAllEvents(location);
 
   // const uluru = {lat: location.lat, lng: location.lng};
-  const uluru = {lat: 34.200432, lng: -118.478111};
+  const uluru = location;
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     center: uluru,
@@ -22,13 +22,44 @@ async function initMap() {
   });
 }
 
-function getAllEvents() {
+function getAllEvents(location) {
   return new Promise((resolve, reject) => {
-    $.ajax('/api/events', {
-      type: 'GET',
-    }).then(events => {
-      // console.log(events);
+    const lat = location.lat;
+    const lng = location.lng;
+    $.get('/api/events?lat=' + lat + '&lng=' + lng)    
+    .then(events => {
       resolve(events);
     })
   })
 }
+
+$('.upVote').click(function(e) {
+  e.preventDefault;
+  const theEvent = $(this)[0].attributes[2].value;
+  $.get('/api/events/' + theEvent, (data) => {
+    const searchedEvent = data[0];
+    const updatedData = {
+      address: searchedEvent.address,
+      city: searchedEvent.city,
+      dateTime: searchedEvent.dateTime,
+      description: searchedEvent.description,
+      id: searchedEvent.id,
+      lat: searchedEvent.lat,
+      lng: searchedEvent.lng,
+      name: searchedEvent.name,
+      state: searchedEvent.state,
+      upVotes: searchedEvent.upVotes + 1,
+      urlPhoto: searchedEvent.urlPhoto,
+      zipCode: searchedEvent.zipCode,
+    }
+
+    $.ajax({
+      type: 'PUT',
+      url: '/api/events',
+      data: updatedData,
+    })
+    .done(function(data) {
+      location.reload();
+    })
+  })
+})
